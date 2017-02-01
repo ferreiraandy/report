9bd74f66a8938b9e58f4cffcd29fd0c77be36a4d
ferreiraandy <andyferreira92@gmail.com>
2017-02-01 13:31:05 -0200

    New project

---
+++ report/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Report::Application.config.secret_key_base = 'c25b32d4b467eba08eb310fec624c0e181407b629b4a40d48fedac221c22445642460bbfa08ccddd8a3e8539fdedc63addd8d96143a5012475d4b5b80fb682e7'
---
+++ report/config/routes.rb
@@ -0,0 +1,5 @@
+Report::Application.routes.draw do
+  resources :uploads
+
+  root :to => "uploads#index"
+end
