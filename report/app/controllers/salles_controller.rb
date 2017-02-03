52cb03e52956213f702f5e7628a387c20cd33467
ferreiraandy <andyferreira92@gmail.com>
2017-02-02 22:42:05 -0200

    dumb db exist

--- report/app/controllers/salles_controller.rb
+++ report/app/controllers/salles_controller.rb
@@ -1,17 +1,12 @@
 require 'fileutils'
 
 class SallesController < ApplicationController
-  before_action :set_salle, only: [:show, :edit, :update, :destroy]
-
   def index
-    @salles = Salle.all
-  end
-
-  def show
-  end
-
-  def new
     @salle = Salle.new
+  end
+
+  def list_report
+    @salles = Salle.all
   end
 
   def create
@@ -25,36 +20,30 @@ class SallesController < ApplicationController
 
         unless line_num == 1
           col = doc.split("\t")
-
           salle = Salle.create({
             purchaser: col[0],
             description: col[1],
             unit_price: col[2],
             quantity: col[3],
             address: col[4],
-            provider: col[5]
+            provider: col[5],
+            total_price: (col[2] * col[3])
           })
 
-          @salles << salle
           unless salle.errors.messages.blank?
             @errors = salle.errors.messages
             raise ActiveRecord::Rollback, @errors
           end
+
+          @salles << salle
         end
       end
     end
-
-    Rails.logger.warn("\n\n\n Errors #{@errors.inspect}\n\n\n")
-    Rails.logger.warn("\n\n\n Saves #{@salles.inspect}\n\n\n")
 
     render :template => "salles/list_report"
   end
 
   private
-    def set_salle
-      @salle = Salle.find(params[:id])
-    end
-
     def salle_params
       params.require(:salle).permit(:file)
     end
--- report/config/routes.rb
+++ report/config/routes.rb
@@ -1,5 +1,6 @@
 Report::Application.routes.draw do
   resources :salles
 
+  get "list-reports" => "salles#list_report"
   root :to => "salles#index"
 end
